132468174f4673cbb0d53c4eafaf65129067cb92
Nahom <nahomohani@gmail.com>
2023-11-21 21:33:28 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import Footer from './footer';
+import Content from './content';
+import Employees from './employees';
+function App() {
+  return (
+    <>
+    <Header/>
+    <Content/>
+    <Employees/>
+    <Footer/>
+
+    </>
+  );
+}
+
+export default App;
